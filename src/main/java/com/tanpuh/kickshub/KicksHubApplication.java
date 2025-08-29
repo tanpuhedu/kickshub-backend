6d4c5b4549650b1f76fe9df31fe7a9d58b9f917e
tanpuhedu <3122411191@sv.sgu.edu.vn>
2025-08-29 14:46:48 +0700

    init project setup

---
+++ src/main/java/com/tanpuh/kickshub/KicksHubApplication.java
@@ -0,0 +1,13 @@
+package com.tanpuh.kickshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KicksHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KicksHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tanpuh/kickshub/KicksHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanpuh.kickshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KicksHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
